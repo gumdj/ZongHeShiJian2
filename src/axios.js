d1e74cac3a10d0d2659d4d7db85b165da43a99df
gumdj <60602489+gumdj@users.noreply.github.com>
2021-06-23 15:53:51 +0800

    2021.6.23

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 import router from "./router"
 import Element from "element-ui"
 
-// axios.defaults.baseURL = 'http://localhost:8081'
-
 const request = axios.create({
     timeout: 5000,
     headers: {
